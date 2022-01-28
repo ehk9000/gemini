74c0cd3cc90fed8b19e9d4ff1816eddd57725e3f
Erik Konner <erik.konner@stord.com>
2022-01-28 16:31:37 -0700

    Added Warehousing.company schema, warehousing context, controllers, & migration for companies table

---
+++ priv/repo/migrations/20220128232755_create_companies.exs
@@ -0,0 +1,13 @@
+defmodule Inventory.Repo.Migrations.CreateCompanies do
+  use Ecto.Migration
+
+  def change do
+    create table(:companies, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string
+      add :tenant_id, :uuid
+
+      timestamps()
+    end
+  end
+end
