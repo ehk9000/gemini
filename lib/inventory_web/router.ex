7193ea85bef21c0f10c9b90f37c002a512825e25
Erik Konner <erik.konner@stord.com>
2022-02-03 15:08:46 -0700

    Removed scope / pipe route

--- lib/inventory_web/router.ex
+++ lib/inventory_web/router.ex
@@ -5,18 +5,17 @@ defmodule InventoryWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", InventoryWeb do
-    pipe_through :api
 
+
+
+
+  scope "/api", InventoryWeb do
+    pipe_through :api
     get "/companies", CompanyController, :index
     get "/companies/:id", CompanyController, :show
     post "/companies", CompanyController, :create
     patch "/companies/:id", CompanyController, :update
     put "/companies/:id", CompanyController, :update
     delete "/companies/:id", CompanyController, :delete
-  end
-
-  scope "/api", InventoryWeb do
-    pipe_through :api
   end
 end
